005c7ccc10ceba6b4a4cca5212fdb61467350872
adeade95 <57157852+adeade95@users.noreply.github.com>
2020-05-18 22:29:01 +0200

    ora si potrebbe provare per vedere se riceve lo stato del pulsante

--- centralina_client.ino
+++ centralina_client.ino
@@ -21,11 +21,14 @@ IPAddress subnet(255, 255, 255, 0);
 IPAddress primaryDNS(8, 8, 8, 8); //optional
 IPAddress secondaryDNS(8, 8, 4, 4); //optional
 
+//ledpin falsh è il 4
+#define ledpin 4
+
 //Your IP address or domain name with URL path
-const char* serverstate = "http://192.168.0.21/state";
-const char* serverreset = "http://192.168.0.21/reset";
+const char* serverNameState = "http://192.168.0.21/state";
+const char* serverNameReset = "http://192.168.0.21/reset";
 
-
+/*
 const char* serverNameTemp = "http://192.168.4.1/temperature";
 const char* serverNameHumi = "http://192.168.4.1/humidity";
 const char* serverNamePres = "http://192.168.4.1/pressure";
@@ -40,16 +43,44 @@ const char* serverNamePres = "http://192.168.4.1/pressure";
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
 /*#define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
-*/
+
 String temperature;
 String humidity;
 String pressure;
-
+*/
 unsigned long previousMillis = 0;
 const long interval = 5000; 
 
+
+String httpGETRequest(const char* serverName) {
+  HTTPClient http;
+    
+  // Your IP address with path or Domain name with URL path 
+  http.begin(serverName);
+  
+  // Send HTTP POST request
+  int httpResponseCode = http.GET();
+  
+  String payload = "--"; 
+  
+  if (httpResponseCode>0) {
+    Serial.print("HTTP Response code: ");
+    Serial.println(httpResponseCode);
+    payload = http.getString();
+  }
+  else {
+    Serial.print("Error code: ");
+    Serial.println(httpResponseCode);
+  }
+  // Free resources
+  http.end();
+
+  return payload;
+}
+
 void setup() {
   Serial.begin(115200);
+  pinMode(ledpin, OUTPUT);    // sets the digital pin 4 led pin as output
   /*
   // Address 0x3C for 128x64, you might need to change this value (use an I2C scanner)
   if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
@@ -72,6 +103,7 @@ void setup() {
   Serial.println(WiFi.localIP());
 }
 
+
 void loop() {
   
   unsigned long currentMillis = millis();
@@ -79,11 +111,41 @@ void loop() {
   if(currentMillis - previousMillis >= interval) {
      // Check WiFi connection status
     if(WiFi.status()== WL_CONNECTED ){ 
-      temperature = httpGETRequest(serverNameTemp);
+      String inputstate= httpGETRequest(serverNameState);//acquisisco dato da scheda server
+      Serial.print("Dato ricevuto ");
+      Serial.println(inputstate);
+      if(inputstate=="1"){
+        digitalWrite(ledpin, HIGH);
+        Serial.println("connesso pulsante premuto");
+        }
+      if(inputstate=="0"){        
+        digitalWrite(ledpin, LOW);
+        Serial.println("connesso pulsante non premuto");     
+      }
+      if(inputstate!="0" && inputstate!="1")
+        Serial.println("possibili problemi di connessione ocn la scheda");          
+
+        /*
+      switch (inputstate)
+      case "1":
+        digitalWrite(ledpin, HIGH);
+        Serial.println("connesso pulsante premuto");
+        break;
+      case "0":
+        digitalWrite(ledpin, LOW);
+        Serial.println("connesso pulsante non premuto");
+       default:
+        Serial.println("possibili problemi di connessione ocn la scheda");
+        }*/
+        inputstate="2";//resettiamao lo stato
+        
+      
+ 
+     /* temperature = httpGETRequest(serverNameTemp);
       humidity = httpGETRequest(serverNameHumi);
       pressure = httpGETRequest(serverNamePres);
       Serial.println("Temperature: " + temperature + " *C - Humidity: " + humidity + " % - Pressure: " + pressure + " hPa");
-      /*
+      
       display.clearDisplay();
       
       // display temperature
@@ -124,30 +186,4 @@ void loop() {
       Serial.println("WiFi Disconnected");
     }
   }
-}
-
-String httpGETRequest(const char* serverName) {
-  HTTPClient http;
-    
-  // Your IP address with path or Domain name with URL path 
-  http.begin(serverName);
-  
-  // Send HTTP POST request
-  int httpResponseCode = http.GET();
-  
-  String payload = "--"; 
-  
-  if (httpResponseCode>0) {
-    Serial.print("HTTP Response code: ");
-    Serial.println(httpResponseCode);
-    payload = http.getString();
-  }
-  else {
-    Serial.print("Error code: ");
-    Serial.println(httpResponseCode);
-  }
-  // Free resources
-  http.end();
-
-  return payload;
 }
